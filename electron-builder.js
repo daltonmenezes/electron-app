2545ed4cc9ca77f65819ae251a5065d54f24b461
Dalton Menezes <daltonmenezes@outlook.com>
2022-03-06 16:48:35 -0300

    feat: release v1.1.0 🎉

--- electron-builder.js
+++ electron-builder.js
@@ -3,13 +3,13 @@ const { APP_CONFIG } = require('./app.config')
 const { NAME, AUTHOR, TITLE, DESCRIPTION, FOLDERS } = APP_CONFIG
 
 const CURRENT_YEAR = new Date().getFullYear()
-const AUTHOR_IN_KEBAB_CASE = AUTHOR.replace(/\s+/g, '-')
+const AUTHOR_IN_KEBAB_CASE = AUTHOR.name.replace(/\s+/g, '-')
 const APP_ID = `com.${AUTHOR_IN_KEBAB_CASE}.${NAME}`.toLowerCase()
 
 module.exports = {
   appId: APP_ID,
   productName: TITLE,
-  copyright: `Copyright © ${CURRENT_YEAR} — ${AUTHOR}`,
+  copyright: `Copyright © ${CURRENT_YEAR} — ${AUTHOR.name}`,
 
   directories: {
     app: FOLDERS.DEV_TEMP_BUILD,
