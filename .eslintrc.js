2443b1e6930a820b874d4e33f8deeb3e8a3b0da4
Jasper <daniel.j.cowley@hotmail.co.uk>
2022-10-30 23:00:27 +0000

    Few code style changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,46 @@
 module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-  },
-  extends: ['prettier', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    '@typescript-eslint/no-empty-interface': 0,
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        tabWidth: 2,
-        useTabs: false,
-        trailingComma: 'es5',
-      },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		es6: true, // enables es6 features
+		jest: true,
+	},
+	extends: ['prettier', 'plugin:prettier/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		'@typescript-eslint/no-empty-interface': 0,
+		'prettier/prettier': [
+			'error',
+			{
+				semi: false,
+				singleQuote: true,
+				tabWidth: 2,
+				useTabs: true,
+				trailingComma: 'es5',
+			},
+		],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'no-tabs': 'off',
+		'max-len': [
+			'error',
+			{
+				code: 150,
+			},
+		],
+		'no-console': [
+			'error',
+			{
+				allow: ['error'],
+			},
+		],
+		'no-underscore-dangle': 'off',
+	},
 }
